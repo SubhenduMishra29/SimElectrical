4085540185e6cdc24aeb7567c419d76fa611af47
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2022-03-14 12:53:40 +0530

    Add files via upload

---
+++ lib/pythonlib/py_test.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+#include <Python.h>
+
+int main()
+{
+	PyObject* pInt;
+
+	Py_Initialize();
+
+	PyRun_SimpleString("import pandapower as pp");
+	PyRun_SimpleString("net = pp.create_empty_network(sn_mva = 100 )");
+	PyRun_SimpleString("print(net)");
+	Py_Finalize();
+
+	printf("\nPress any key to exit...\n");
+	if(!_getch()) _getch();
+	return 0;
+}
