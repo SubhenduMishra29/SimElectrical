702fc7f7fa460b24611349d4a70545eed5e5575b
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2022-03-19 16:01:24 +0530

    Add files via upload

---
+++ lib/pythonlib/readfile.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <Python.h>
+#include <pyhelper.hpp>
+
+int main()
+{
+	CPyInstance hInstance;
+
+	CPyObject pName = PyUnicode_FromString("pyemb3");
+	CPyObject pModule = PyImport_Import(pName);
+
+	if(pModule)
+	{
+		CPyObject pFunc = PyObject_GetAttrString(pModule, "getInteger");
+		if(pFunc && PyCallable_Check(pFunc))
+		{
+			CPyObject pValue = PyObject_CallObject(pFunc, NULL);
+
+			printf_s("C: getInteger() = %ld\n", PyLong_AsLong(pValue));
+		}
+		else
+		{
+			printf("ERROR: function getInteger()\n");
+		}
+
+	}
+	else
+	{
+		printf_s("ERROR: Module not imported\n");
+	}
+
+	return 0;
+}
