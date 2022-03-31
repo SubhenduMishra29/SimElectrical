1990f8a97ba416e0537715e630f25128fda86acc
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2022-03-31 21:23:21 +0530

    Add files via upload

--- source/WindowManager/src/main.c
+++ source/WindowManager/src/main.c
@@ -108,18 +108,24 @@ G_MODULE_EXPORT void on_Gen_Toolbar_cut_clicked(){
 }
 G_MODULE_EXPORT gboolean on_menuItem_file_new_button_press_event (GtkWidget *caller){
 	GtkWindow *parent_window = GTK_WINDOW(caller);
-	    GtkFileChooserNative *native;
-	    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SAVE;
-	    gint res;
+	GtkFileChooserNative *native;
+	GtkFileFilter *filter;
+	GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SAVE;
+	gint res;
 
-	    native = gtk_file_chooser_native_new("Save File",
+	native = gtk_file_chooser_native_new("Save File",
 		                                 parent_window,
 		                                 action,
 		                                 "_Save",
 		                                 "_Cancel");
+	filter = gtk_file_filter_new();
+    	gtk_file_filter_add_pattern(filter, "*.cpp");
+    	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(native), filter);
+    	gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(native), "test.cpp");
+    	//gtk_widget_show_all(native);
 
-	    res = gtk_native_dialog_run(GTK_NATIVE_DIALOG(native));
-	    if (res == GTK_RESPONSE_ACCEPT)
+	res = gtk_native_dialog_run(GTK_NATIVE_DIALOG(native));
+	if (res == GTK_RESPONSE_ACCEPT)
 	    {
 		char *filename;
 		GtkFileChooser *chooser = GTK_FILE_CHOOSER(native);
@@ -131,7 +137,7 @@ G_MODULE_EXPORT gboolean on_menuItem_file_new_button_press_event (GtkWidget *cal
 		g_free(filename);
 	    }
 
-	    g_object_unref(native);
+	g_object_unref(native);
 	 
 }
 G_MODULE_EXPORT void on_menuItem_help_about_select (){
