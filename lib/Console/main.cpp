b394cac52c84cc768019d8c046d7015c4b72cd64
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-04-23 11:21:32 +0530

    Update main.cpp

--- lib/Console/main.cpp
+++ lib/Console/main.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 #include <cstdio>
-#include "blif.tab.h"
 
-extern FILE* yyin;
-extern int yyparse();
-
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <input_file.blif>" << std::endl;
-        return 1;
-    }
-
-    // Open the input BLIF file
-    FILE* inputFile = fopen(argv[1], "r");
-    if (!inputFile) {
-        std::cerr << "Error: Failed to open input file " << argv[1] << std::endl;
-        return 1;
-    }
-
-    // Set the input file for the lexer
-    yyin = inputFile;
-
-    // Parse the BLIF file
-    yyparse();
-
-    // Close the input file
-    fclose(inputFile);
-
-    return 0;
+extern "C" {
+    int yyparse();
+    FILE* yyin;
 }
 
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    yyin = fopen(argv[1], "r");
+    if (!yyin) {
+        std::cerr << "Error opening file: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    int result = yyparse();
+    fclose(yyin);
+
+    if (result == 0) {
+        std::cout << "Parsing successful!" << std::endl;
+    } else {
+        std::cerr << "Parsing failed!" << std::endl;
+    }
+
+    return result;
+}
