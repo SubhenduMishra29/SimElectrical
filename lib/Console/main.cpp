40c648797078156628dc8ca052c946b84ba47b27
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-04-23 11:17:53 +0530

    Update main.cpp

--- lib/Console/main.cpp
+++ lib/Console/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
-#include "blif_parser.tab.h"
+#include <cstdio>
+#include "blif.tab.h"
 
 extern FILE* yyin;
 extern int yyparse();
 
-int main() {
-    yyin = fopen("input.blif", "r");
-    if (!yyin) {
-        std::cerr << "Error: Failed to open input file." << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file.blif>" << std::endl;
         return 1;
     }
 
+    // Open the input BLIF file
+    FILE* inputFile = fopen(argv[1], "r");
+    if (!inputFile) {
+        std::cerr << "Error: Failed to open input file " << argv[1] << std::endl;
+        return 1;
+    }
+
+    // Set the input file for the lexer
+    yyin = inputFile;
+
+    // Parse the BLIF file
     yyparse();
 
-    fclose(yyin);
+    // Close the input file
+    fclose(inputFile);
 
     return 0;
 }
